44b065cc86f9149a69ec50b9d83771f08e5d09fb
empchief <bjornmagnekristensen1999@gmail.com>
2024-08-12 15:18:45 +0200

    back and programmed a bit

--- Toralizer/toralizer.c
+++ Toralizer/toralizer.c
@@ -1,32 +1,123 @@
 /*Toralizer.c*/
 #include "toralizer.h"
 
+// Function to create a proxy request
+Request *create_request(const char *destination_address, const int destination_port)
+{
+    Request *new_request;
+
+    new_request = malloc(REQUEST_SIZE);
+    if (new_request == NULL)
+    {
+        perror("Error: Memory allocation failed");
+        return NULL;
+    }
+
+    new_request->version = 4;
+    new_request->command = 1;
+    new_request->destination_port = htons(destination_port);
+    new_request->destination_address = inet_addr(destination_address);
+    strncpy((char *)new_request->userid, USERNAME, 8);
+
+    return new_request;
+}
 
 int main(int argc, char *argv[])
 {
     if (argc != 3)
     {
-        fprintf(stderr, "Usage: %s <ip> <port>\n", argv[0]);
+        fprintf(stderr, "Usage: %s <IP_ADDRESS> <PORT>\n", argv[0]);
         return -1;
     }
 
-    char *host_ip = argv[1];
-    int host_port = atoi(argv[2]);
-    int sockfd;
-    struct sockaddr_in sockets_a;
+    char *ip_address = argv[1];
+    int port = atoi(argv[2]);
+    int socket_fd;
+    struct sockaddr_in server_address;
+    Request *request;
+    Response *response;
+    char buffer[RESPONSE_SIZE];
+    int success;
 
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0)
+    // Create socket
+    socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (socket_fd < 0)
     {
-        perror("socket creation failed");
+        perror("Error: Socket creation failed");
         return -1;
     }
 
-    sockets_a.sin_family = AF_INET;
-    sockets_a.sin_port = htons(host_port);
-    sockets_a.sin_addr.s_addr = inet_addr(host_ip);
-    connect(sockfd, (struct sockaddr *)&sockets_a, sizeof(sockets_a));
-    
+    // Setup server address struct
+    memset(&server_address, 0, sizeof(server_address));
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(port);
+
+    // Convert IP address from text to binary form
+    if (inet_pton(AF_INET, ip_address, &server_address.sin_addr) <= 0)
+    {
+        perror("Error: Invalid IP address / Address not supported");
+        close(socket_fd);
+        return -1;
+    }
+
+    // Connect to server
+    if (connect(socket_fd, (struct sockaddr *)&server_address, sizeof(server_address)) < 0)
+    {
+        perror("Error: Connection failed");
+        close(socket_fd);
+        return -1;
+    }
+
+    printf("Connected to %s:%d\n", ip_address, port);
+
+    // Create the request
+    request = create_request(ip_address, port);
+    if (request == NULL)
+    {
+        fprintf(stderr, "Error: Request creation failed\n");
+        close(socket_fd);
+        return -1;
+    }
+
+    // Send the request
+    ssize_t bytes_written = write(socket_fd, request, REQUEST_SIZE);
+    if (bytes_written < 0)
+    {
+        perror("Error: Write failed");
+        free(request);
+        close(socket_fd);
+        return -1;
+    }
+
+    printf("Request sent successfully (%ld bytes)\n", bytes_written);
+
+    memset(buffer, 0, RESPONSE_SIZE);
+    ssize_t bytes_read = read(socket_fd, buffer, RESPONSE_SIZE);
+    if (bytes_read < 0)
+    {
+        perror("Error: Read failed");
+        free(request);
+        close(socket_fd);
+        return -1;
+    }
+
+    response = (Response *)buffer;
+    success = (response->command == 90);
+
+    if (!success)
+    {
+        fprintf(stderr, "Unable to traverse the proxy, error code: %d\n", response->command);
+        free(request);
+        close(socket_fd);
+        return -1;
+    }
+
+    printf("Successfully connected to the proxy to %s:%d\n", ip_address, port);
+
+    // Close the socket
+    close(socket_fd);
+
+    free(request);
 
     return 0;
 }
--- Toralizer/toralizer.h
+++ Toralizer/toralizer.h
@@ -1,6 +1,4 @@
-#ifndef TORALIZER_H
-#define TORALIZER_H
-
+/*toralizer.h*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -11,27 +9,35 @@
 
 #define PROXY_IP "127.0.0.1"
 #define PROXY_PORT 9050
+#define REQUEST_SIZE sizeof(struct proxy_request)
+#define RESPONSE_SIZE sizeof(struct proxy_response)
+#define USERNAME "toraliz"
 
 typedef unsigned char int8;
 typedef unsigned short int int16;
 typedef unsigned int int32;
 
-struct proxy_request{
-    int8 version;
-    int8 command;
-    int16 destination_port;
-    int32 destination_address;
-    unsigned char userid[8];
+// Structure to hold a proxy request
+struct proxy_request
+{
+    int8 version;                 // Version of the request
+    int8 command;                 // Command type
+    int16 destination_port;       // Destination port number
+    int32 destination_address;    // Destination IP address
+    unsigned char userid[8];      // User ID
 };
 typedef struct proxy_request Request;
 
-struct proxy_response{
-    int8 version;
-    int8 command;
-    int16 destination_port;
-    int32 destination_address;
+// Structure to hold a proxy response
+struct proxy_response
+{
+    int8 version;                 // Version of the response
+    int8 command;                 // Command type
+    int16 destination_port;       // Destination port number
+    int32 destination_address;    // Destination IP address
 };
 typedef struct proxy_response Response;
 
-
-#endif
+// Function prototypes
+Request *create_request(const char *, const int);
+int main(int, char **);
